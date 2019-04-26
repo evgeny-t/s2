ae891e1fd8f68849c4861a0ece7b44ae607ce928
etimoshenko <timoshenko.evgeny@gmail.com>
2019-04-26 20:07:53 +0300

    new index route

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-export default () => (
+export default (props) => (
   <div style={{ textAlign: 'center' }}>
+    <code>{JSON.stringify(props, null, 2)}</code>
     <h1>Welcome to React-Static</h1>
   </div>
 )
